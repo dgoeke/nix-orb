a437e029f20b94ae34ceb2c50dd485bd309f1dff
David Goeke <davidg@circleci.com>
2018-04-24 13:21:11 -0700

    Switch to haskell

--- default.nix
+++ default.nix
@@ -1,23 +1,3 @@
 { pkgs ? import ./pkgs.nix }:
 
-pkgs.stdenv.mkDerivation {
-  name = "hello-clojure";
-  src = ./.;
-  buildInputs = [
-    pkgs.leiningen
-    pkgs.openjdk
-  ];
-  buildPhase = ''
-    lein uberjar
-  '';
-  checkPhase = ''
-    lein test
-  '';
-  installPhase = ''
-    mkdir -p $out/bin
-    cp -r target/uberjar/hello-clojure-0.1.0-SNAPSHOT-standalone.jar $out/hello-clojure.jar
-    echo "#!${pkgs.bash}/bin/bash" > $out/bin/hello-clojure
-    echo "${pkgs.openjdk}/bin/java -jar $out/hello-clojure.jar" >> $out/bin/hello-clojure
-    chmod +x $out/bin/hello-clojure
-  '';
-}
+pkgs.haskellPackages.callPackage ./app.nix { }
--- src/hello_clojure/core.clj
+++
@@ -1,5 +0,0 @@
-(ns hello-clojure.core
-  (:gen-class))
-
-(defn -main [& args]
-  (println "Hello, Clojure"))
